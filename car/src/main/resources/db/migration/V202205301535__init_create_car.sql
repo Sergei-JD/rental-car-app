a27b1df2bed266ec329acb5bf1f307fcfeb977ab
Sergei-JD <sergshlyazhko@gmail.com>
2022-05-30 15:43:05 +0400

    Initial commit

---
+++ car/src/main/resources/db/migration/V202205301535__init_create_car.sql
@@ -0,0 +1,21 @@
+-- -----------------------------------------------------
+-- Table catalog
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS car_catalog
+(
+    car_catalog_id      SERIAL PRIMARY KEY,
+    registration_number VARCHAR(8)   NOT NULL,
+    car_type            VARCHAR(32),
+    year_of_manufacture CHAR(4)      NOT NULL,
+    make                VARCHAR(128) NOT NULL,
+    model               VARCHAR(128) NOT NULL,
+    colour              VARCHAR(32)  NOT NULL,
+    price               DECIMAL      NOT NULL,
+    car_status          VARCHAR(32)  NOT NULL,
+    order_id            BIGINT       NOT NULL
+
+    );
+
+-- DROP TABLE car_catalog;
+
+COMMIT;
